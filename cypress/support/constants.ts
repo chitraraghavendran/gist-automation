fbc98db35fd1ea698ea8eb8a910fd963395249df
Chitra Raghavendran <chitraraghavendran@Chitras-MacBook-Air.local>
2025-01-06 10:52:47 +0100

    Adding GitHub Workflows

--- cypress/support/constants.ts
+++ cypress/support/constants.ts
@@ -1,7 +1,6 @@
 export const BASE_URL = 'https://api.github.com';
 export const GitHub_URL = 'https://github.com/login';
 
-// Ideally should be extracted from Cypredd ENV variables.
-export const BEARER_TOKEN = 'removed';
-export const GITHUB_USERNAME = 'chitraraghavendran.1991@gmail.com';
-export const GITHUB_PASSWORD = 'OmSai@2891';
+export const BEARER_TOKEN = Cypress.env('API_BEARER_TOKEN');
+export const GITHUB_USERNAME = Cypress.env('UI_USERNAME');
+export const GITHUB_PASSWORD = Cypress.env('UI_PASSWORD');
