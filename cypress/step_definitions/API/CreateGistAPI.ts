c4d56afb1effb021474e6a424ef5bb50e332623e
Chitra Raghavendran <chitraraghavendran@Chitras-MacBook-Air.local>
2025-01-07 01:21:33 +0100

    Adding Github workflow-9

--- cypress/step_definitions/API/CreateGistAPI.ts
+++ cypress/step_definitions/API/CreateGistAPI.ts
@@ -1,13 +1,13 @@
 /* eslint-disable */
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
-import { BASE_URL, BEARER_TOKEN } from '../../support/constants';
+import { BASE_URL, BEARER_TOKEN, GITHUB_PASSWORD, GITHUB_USERNAME } from '../../support/constants';
 
 let apiEndpoint: string ;
 let gistID: any;
 
 Given ('the API endpoint for create Gist', () => {
-    apiEndpoint = BASE_URL + '/gists';
-    cy.log('API Bearer Token Is '+ BEARER_TOKEN);
+    apiEndpoint = BASE_URL + '/' + BEARER_TOKEN + '/' + GITHUB_USERNAME + '/' + GITHUB_PASSWORD;
+    console.log('API Bearer Token Is '+BEARER_TOKEN);
 });
 
 When('I send POST request with request payload as {string}', requestBodyFilePath => {
